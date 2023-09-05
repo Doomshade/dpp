a4cb25802b12ea4a5a5d01adb128a27c72ea41e8
Jakub Šmrha <jsmahy@civ.zcu.cz>
2023-09-05 15:59:54 +0200

    Introduce parser
    Signed-off-by: Jakub Šmrha <jsmahy@civ.zcu.cz>

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use std::fs;
 use crate::lexer::Lexer;
 
 mod lexer;
+mod parse;
 
 fn main() {
     let mut lexer = Lexer::default();
-    let file = fs::read_to_string("examples/main.dpp").expect("Should have been able to read the \
-    file");
+    let file = fs::read_to_string("examples/main.dpp")
+        .expect("Should have been able to read the file");
 
     let vec = lexer.lex(file.as_str());
     println!("{:?}", vec);
