4c607e978a315039327972c60f10a792c14ba354
Doomshade <kuba.smrha@gmail.com>
2023-09-18 19:39:36 +0200

    Add all clippy warnings lol

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
+#![warn(
+    clippy::all,
+    clippy::restriction,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::cargo
+)]
+use crate::emitter::Emitter;
 use crate::lexer::Lexer;
 use crate::parser::Parser;
 use std::fs;
 use std::fs::File;
-use crate::emitter::Emitter;
 
 mod ast;
+mod emitter;
 mod lexer;
 mod parser;
-mod emitter;
 
 fn main() {
-    let file =
-        fs::read_to_string("examples/first_simple_example.dpp").expect("Should have been able to read the file");
+    let file = fs::read_to_string("examples/first_simple_example.dpp")
+        .expect("Should have been able to read the file");
 
     let lexer = Lexer::new(file.as_str());
     let mut parser = Parser::new(lexer);
@@ -19,7 +26,11 @@ fn main() {
     parser.print_parse_tree();
     let mut emitter = Emitter::new(program);
     let file_name = String::from("out/dpp/first_simple_example.asm");
-    let file = File::create(&file_name).unwrap_or_else(|er| panic!("Failed to create file \
-    {file_name}: {er}"));
+    let file = File::create(&file_name).unwrap_or_else(|er| {
+        panic!(
+            "Failed to create file \
+    {file_name}: {er}"
+        )
+    });
     dbg!(emitter.emit(&file)).expect("Failed to emit assembly code");
 }
