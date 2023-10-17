1dbe185acd955dfce0d8aee82e963a2a441b9623
Doomshade <kuba.smrha@gmail.com>
2023-10-17 21:42:47 +0200

    Implement PosMacro

--- dpp_macros/dpp_macros_derive/src/lib.rs
+++ dpp_macros/dpp_macros_derive/src/lib.rs
@@ -3,6 +3,27 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn;
 
+#[proc_macro_derive(PosMacro)]
+pub fn pos_macro_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_pos_macro(&ast)
+}
+
+fn impl_pos_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl<'a> PosMacro for #name<'a> {
+            fn row(&self) -> u32 {
+                self.position.0
+            }
+            fn col(&self) -> u32 {
+                self.position.1
+            }
+        }
+    };
+    gen.into()
+}
+
 #[proc_macro_derive(HelloMacro)]
 pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
     // Construct a representation of Rust code as a syntax tree
--- dpp_macros/src/lib.rs
+++ dpp_macros/src/lib.rs
@@ -1,3 +1,8 @@
 pub trait HelloMacro {
     fn hello_macro();
 }
+
+pub trait PosMacro {
+    fn row(&self) -> u32;
+    fn col(&self) -> u32;
+}
