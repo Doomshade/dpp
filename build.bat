e1b9036b84b61d04a6a212a992996278f83f29d7
Doomshade <kuba.smrha@gmail.com>
2023-11-12 18:30:46 +0100

    Update README and cleanup project

--- build.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-commonvars=--color=always --package dpp --bin dpp
-cargo build $commonvars
-cargo run $commonvars
-cd out/dpp
-nasm -f win32 first_simple_example.asm
-gcc first_simple_example.obj -g
-a.exe
